1d1e18211c6119cba36882a43fa7df4f3c9f9264
David Calavera <david.calavera@gmail.com>
2016-10-17 15:57:38 -0700

    Use a better welcome screen.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/View.elm
+++ src/View.elm
@@ -1,6 +1,6 @@
 module View exposing (..)
 
-import Html exposing (Html, div, text, main')
+import Html exposing (Html, div, text, main', p)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Messages exposing (Msg(..))
@@ -29,8 +29,10 @@ notFoundView model =
     div []
         [ Partials.pageHeader model Nothing
         , main'
-            []
-            [ text "Not found" ]
+            [ class "central-message" ]
+            [ div [ class "title" ]
+                [ text "This is not the place you're looking for" ]
+            ]
         ]
 
 
@@ -39,8 +41,14 @@ homeView model =
     div []
         [ Partials.pageHeader model Nothing
         , main'
-            []
-            [ text "Welcome to Netlify's Playground" ]
+            [ class "central-message" ]
+            [ div [ class "title" ]
+                [ text "PLAY - Netlify's Playground" ]
+            , p [] [ text "Version 1.0.0" ]
+            , p [] [ text "By David Calavera et al." ]
+            , p [] [ text "Modified by hello@netlify.com" ]
+            , p [] [ text "Play is open source and freely distributable" ]
+            ]
         ]
 
 
