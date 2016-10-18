11537c55d9269ea9b3a6758104bf4d04be2003ac
David Calavera <david.calavera@gmail.com>
2016-10-17 20:20:57 -0700

    Add short explanation about content.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/View.elm
+++ src/View.elm
@@ -143,14 +143,14 @@ redirectsResultHeader response =
 
 
 redirectsPlaceholder =
-    """# Redirect rules, use # to write comments
+    """# Type here the redirect rules you want to check.
+# You can use hashtags to write comments.
+# Examples:
 
-# Book rules:
 /books/grove-high-output-management.html      /books/grove/high-output-management      301    # Simple redirect
 /books/grove-only-the-paranoid-survive.html   /books/grove/only-the-paranoid-survive   301!   # Forced redirect
 /books/meadowns-thinking-in-systems.html      /books/meadowns/thinking-in-systems      303    # See other redirect
 
-# Blog rules:
 /the-art-of-closing   https://blog.jessfraz.com/post/the-art-of-closing     200    # Proxy rule, browser location won't change, only available in PRO plans
 /a-nerd-in-a-cave     http://randsinrepose.com/archives/a-nerd-in-a-cave/   200!   # Forced proxy rule
 """
